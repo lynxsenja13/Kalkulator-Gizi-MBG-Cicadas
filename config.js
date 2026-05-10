0afdcd243591087e79b4743eab34455bf7d84a04
lynxsenja13 <lynxsenja13@gmail.com>
2026-05-10 09:09:22 +0700

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzhCqzKM5xUaR1jIHS-1aWrGcG_78jBqI-y5RnEQMnx0Rp2hFtxiQqE2WoIVzbKf4gp/exec";
-const API_URL2 = "https://script.google.com/macros/s/AKfycbwVzxjOE5pXuAahXNWR202BkYokorW2rO9bxBp-TXcmcw_tPoyhS_kqQG4vHzh-0ZWq/exec";
